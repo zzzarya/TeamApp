ac91271da1edf791a95e82c1462969c2606c9b8b
Anton Zarichny <antonzarichny@gmail.com>
2022-09-11 15:56:20 +0300

    Добавил дисмисс

--- TeamApp/ProductViewViewController.swift
+++ TeamApp/ProductViewViewController.swift
@@ -93,20 +93,18 @@ class ProductViewViewController: UIViewController {
     
     @IBAction func buttonToBasketPressed() {
         chooseProduct()
-    }
-    
-    
-    
-    /*
-    // MARK: - Navigation
+        dismiss(animated: true)
+        
+        /*
+        // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // Get the new view controller using segue.destination.
+            // Pass the selected object to the new view controller.
+        }
+        */
     }
-    */
-
 }
 
 extension ProductViewViewController {
