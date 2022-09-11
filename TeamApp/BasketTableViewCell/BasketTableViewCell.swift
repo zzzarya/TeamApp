d7cc3e74c232b81288a90d4852c4d6372f0a9847
DICSA <belovrl1@gmail.com>
2022-09-11 19:21:51 +0300

    Сторибоар без папки

--- TeamApp/BasketTableViewCell/BasketTableViewCell.swift
+++ TeamApp/BasketTableViewCell/BasketTableViewCell.swift
@@ -25,6 +25,7 @@ class BasketTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+        
         // Configure the view for the selected state
     }
     
