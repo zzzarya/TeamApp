f7a684d5bc3bd541a8e8054f189dba298ce9a2b4
DICSA <belovrl1@gmail.com>
2022-09-11 14:00:33 +0300

    Добавлен и настроен сторибоард для корзины созданна кастомная ячейка. Осталось связать кастомную ячейку со сторибордом и настроить логику

---
+++ TeamApp/TableView/BasketTVCell/BasketTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BasketTableViewCell.swift
+//  TeamApp
+//
+//  Created by Белов Руслан on 11.09.2022.
+//
+
+import UIKit
+
+class BasketTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TeamApp/ViewControllers/BasketViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BasketViewController.swift
+//  TeamApp
+//
+//  Created by Белов Руслан on 10.09.2022.
+//
+
+import UIKit
+
+class BasketViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
